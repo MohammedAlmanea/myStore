4e3414318769ca7a5fabfa0ece177a1ef92d1d2e
MohammedAlmanea <MohammedAlmanea@outlook.com>
2022-12-17 21:37:01 +0300

    Changed product-item-detail component class used cart,product,route Services to show the product and add it to the cart

--- src/app/components/product-item-detail/product-item-detail.component.ts
+++ src/app/components/product-item-detail/product-item-detail.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CartService } from 'src/app/services/cart/cart.service';
+import { ProductService } from 'src/app/services/product/product.service';
 
 @Component({
   selector: 'app-product-item-detail',
   templateUrl: './product-item-detail.component.html',
-  styleUrls: ['./product-item-detail.component.css']
+  styleUrls: ['./product-item-detail.component.css'],
 })
 export class ProductItemDetailComponent {
+  id: number = 0;
+  product: Product = {
+    id: 0,
+    name: '',
+    url: '',
+    description: '',
+    price: 0,
+  };
 
+  constructor(
+    private cartService: CartService,
+    private productService: ProductService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    // The following tutorial was used for this part
+    // https://betterprogramming.pub/angular-6-url-parameters-860db789db85
+    this.id = parseInt(this.route.snapshot.paramMap.get('id') as string);
+    this.productService.getProduct(this.id).subscribe((data) => {
+      this.product = data[0];
+    });
+  }
+
+  addToCart(item: Product, quantity: string) {
+    this.cartService.addToCart(item, parseInt(quantity));
+  }
 }
