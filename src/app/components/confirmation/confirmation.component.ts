3676e60ae817ed34eb7f8bf2595b3402ad45eebc
MohammedAlmanea <MohammedAlmanea@outlook.com>
2022-12-17 21:19:32 +0300

    Modified confirmation component used order service to get the order

--- src/app/components/confirmation/confirmation.component.ts
+++ src/app/components/confirmation/confirmation.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { OrderService } from 'src/app/services/order/order.service';
 
 @Component({
   selector: 'app-confirmation',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./confirmation.component.css']
 })
 export class ConfirmationComponent {
+order: Order = {
+  id: 0,
+  name: '',
+  address: '',
+  cart: [],
+  total: 0
+}
 
+constructor(private orderService: OrderService) {}
+
+ngOnInit() :void {
+  this.order = this.orderService.getOrder()
+}
 }
